a5c6c903e49d9071725d7876f5be32242b25b923
viswanath <mekala.viswanathreddy@gmail.com>
2018-04-21 20:27:32 +0530

    distrubution management

---
+++ src/main/java/ECOMMERS/App.java
@@ -0,0 +1,16 @@
+package ebay;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello world!" );
+
+
+        System.out.println( "Hello world!" );
+
+        System.out.println( "Hello java world!" );
+		
+    }
+}
